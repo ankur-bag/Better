05be2d1e1bb367d605c70ea47eb2f7e84dded08f
Ankur Bag <ankurbag700@gmail.com>
2026-05-10 15:42:13 +0530

    gitignore python version and test files; fix protobuf issue via env var

--- Backend/run.py
+++ Backend/run.py
@@ -1,5 +1,6 @@
 """Flask application entry point."""
 import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 from app import create_app
 
 if __name__ == '__main__':
--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,5 +1,6 @@
-from app import create_app
 import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+from app import create_app
 
 config_name = os.getenv('FLASK_ENV', 'production')
 app = create_app(config_name)
